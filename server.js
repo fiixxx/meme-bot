e8604ab7f9a23f71d9d7287639c6e1f8d25ff614
Glitch (hello-express) <none>
2020-03-23 08:39:49 +0000

    🕶🍳 Checkpoint ./server.js:9765319/14

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ client.on("ready", () => {
     `Ashqtm ta mrdn`,
     `😘 A 😘`,
     `7 A B O UP`
+    ``,
   ];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
