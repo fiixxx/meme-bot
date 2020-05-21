2bf6f42d81345a4d48d7bba05199b79645995fdf
Glitch (dull-boundless-link) <none>
2020-05-21 22:02:45 +0000

    📡⛲️ Checkpoint ./server.js:14058352/10

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ client.commands = new Collection();
 client.aliases = new Collection();
 client.on("ready", () => {
   console.log(`Online In Servers : ${client.guilds.size} `);
-  let statuses = [`?help`,
+  let statuses = [`dyno.gg|?help`,
     
     
 `Servers: ${client.guilds.size} | Users: ${client.users.size}`, 
