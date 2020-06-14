ffee4785853791a1269418ff3a2ae67031d14bc9
Glitch (dull-boundless-link) <none>
2020-06-14 17:05:43 +0000

    ⛵️⏱ Checkpoint ./server.js:16793140/121

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/////ئەلێرەدا دەستکاری هیچ شتێک مەکە تەنها ئەوانەی خوارەوەنەبێت
+
 const http = require("http");
 const express = require("express");
 const app = express();
@@ -57,6 +57,5 @@ client.on('ready',async () => { console.log("Starting.."); let g = client.guilds
 
 
 client.login("NjM1MDIyODgyMTYxODE5NjU4.XuTrDg.iu1ZX6XUCJY6ZlZ6WdoAPqys5c8");/////لێرە تۆکینیەکە دانێ
-//Bymars
-////////////☝ئەلێرەدا تۆکینی ئەکانت دانێ
+//Bymars     ێ
 
